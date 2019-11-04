985c125dc314229328c22568aab70b8d7c56501c
Milo Rupp <badtzmaru139@gmail.com>
2019-11-03 17:51:34 -0800

    Create basic navbar, make logout interface functional

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,73 @@
     <!-- <div id="nav">
       <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
-    </div> -->
-    <router-view/>
+    </div>-->
+    <nav class="flex items-center justify-between bg-teal-500 p-4">
+      <div class="flex items-center flex-shrink-0 text-white mr-6">
+        <span class="font-semibold text-xl tracking-tight">Panel</span>
+      </div>
+      <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+        <div class="text-sm lg:flex-grow">
+          <router-link
+            to="/"
+            class="inline-block mt-0 text-teal-200 hover:text-white mr-4"
+          >Home</router-link>
+        </div>
+        <div class="flex">
+          <div class="block text-sm px-4 py-2 text-teal-200">{{ user.email}}</div>
+          <button
+            v-if="Object.keys(user).length != 0"
+            class="block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+            @click="logout"
+          >Logout</button>
+        </div>
+      </div>
+    </nav>
+    <router-view />
   </div>
 </template>
 
+<script lang="ts">
+import Vue from 'vue';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+
+export default Vue.extend({
+  name: 'App',
+  components: {},
+  data() {
+    return {
+      user: {},
+    };
+  },
+  methods: {
+    logout() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          this.$router.replace('login');
+        });
+    },
+  },
+  mounted() {
+    console.log(this.user);
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.user = user;
+        console.log(user.uid);
+      } else {
+        this.user = {};
+      }
+    });
+  },
+});
+</script>
+
 <style>
-html,body,#app {
+html,
+body,
+#app {
   height: 100%;
   width: 100%;
   margin: 0;
