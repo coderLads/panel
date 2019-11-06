eea15cae233c6d58e0811477a7f56817fa0bb835
Milo Rupp <badtzmaru139@gmail.com>
2019-11-06 12:32:35 -0800

    Update code and css style to match project specs

--- src/components/tiles/WeatherTile.vue
+++ src/components/tiles/WeatherTile.vue
@@ -5,8 +5,12 @@
     class="w-full h-full bg-white text-center m-0 rounded shadow-md flex flex-col"
   >
   <!--<img :src="iconurl" class='block text-center w-12 h-12' style='margin: 0 auto; margin-bottom: -.75rem'>-->
-    <p class="text-4xl text-teal-400 mt-4">{{weather}}º</p>
+    <div class="text-teal-500 mt-4 flex flex-row justify-center">
+      <p class="text-4xl ml-2">{{weather}}</p>
+      <span class="text-xl pb-2">º</span>
+    </div>
     <span class="text-base text-gray-500 mt-1 align-middle">{{location}}</span>
+    <div class="text-sm text-gray-700 mt-1">Open Weather Map</div>
   </a>
 </template>
 
@@ -18,23 +22,23 @@ export default Vue.extend({
   props: {},
   data() {
     return {
-        link: '',
-        weather: 0,
-        location: 'Seattle',
-        iconurl: ''
+      link: '',
+      weather: 0,
+      location: 'Seattle',
+      iconurl: '',
     };
   },
   beforeMount() {
-    let self = this;
-    let request = new XMLHttpRequest();
-    request.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=' + self.location + '&APPID=ccc82db3a17a22d2039288a38a156b12&units=imperial', true);
-    request.onload = function() {
-    if (this.status >= 200 && this.status < 400) {
-        let r = JSON.parse(this.response);
-        self.iconurl = 'http://openweathermap.org/img/wn/' + r.weather[0].icon + '@2x.png';
+    const self = this;
+    const request = new XMLHttpRequest();
+    request.open('GET', `http://api.openweathermap.org/data/2.5/weather?q=${self.location}&APPID=ccc82db3a17a22d2039288a38a156b12&units=imperial`, true);
+    request.onload = () => {
+      if (request.status >= 200 && request.status < 400) {
+        const r = JSON.parse(request.response);
+        self.iconurl = `http://openweathermap.org/img/wn/${r.weather[0].icon}@2x.png`;
         self.weather = Math.round(r.main.temp);
-        self.link = 'https://openweathermap.org/find?q=' + self.location;
-    }
+        self.link = `https://openweathermap.org/find?q=${self.location}`;
+      }
     };
     request.send();
   },
