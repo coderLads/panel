b421109abf41c1b9ac17f185bcd95f8c10704fda
Milo Rupp <badtzmaru139@gmail.com>
2020-05-03 16:28:27 -0700

    begin work on tile picker

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,11 +1,65 @@
 <template>
   <div class="h-full">
-    <div v-if="showPicker === true" class="blur absolute z-10 h-full w-full" @click="quitModal($event)">
+    <div
+      v-if="showPicker === true"
+      class="blur absolute z-10 h-full w-full"
+      @click="quitModal($event)"
+    >
       <div
         v-if="showPicker === true"
-        class="z-20 container mx-auto bg-white rounded max-w-lg p-8 mt-8 shadow-lg"
+        class="z-20 container mx-auto bg-gray-100 rounded max-w-lg p-8 mt-8 shadow-lg"
       >
-        <h3 class="block text-gray-700 text-lg font-bold mb-4"> Pick a tile for slot {{currentTileIndex}}</h3>
+        <h3
+          class="block text-gray-700 text-lg font-bold mb-4 ml-6"
+        >Pick a tile for slot {{currentTileIndex}}</h3>
+        <ul class="list-none flex flex-row flex-wrap justify-around my-4">
+          <li
+            class="text-teal-500 text-base h-16 w-2/5 p-4 m-2 bg-white text-center rounded shadow cursor-pointer"
+            v-for="tileName in Object.keys(templates)"
+            :key="tileName"
+            @click="confirmTile(tileName)"
+          >{{tileName}}</li>
+        </ul>
+      </div>
+    </div>
+    <div
+      v-if="showConfirm === true"
+      class="blur absolute z-10 h-full w-full"
+      @click="quitConfirm($event)"
+    >
+      <div
+        v-if="showConfirm === true"
+        class="z-20 container mx-auto bg-gray-100 rounded max-w-lg p-8 mt-8 shadow-lg"
+      >
+        <h3 class="block text-gray-700 text-lg font-bold mb-4">{{addTileName}}</h3>
+        <div v-if="formError !== ''" class="mb-4">
+          <ul class="text-red-500 font-bold">
+            <li v-for="error in formError" :key="error">{{error}}</li>
+          </ul>
+        </div>
+        <div v-for="field in Object.keys(templates[addTileName])" :key="field">
+          <div
+            v-if="field === 'info'"
+            class="text-base text-gray-500 mb-4"
+          >{{templates[addTileName].info}}</div>
+          <div v-else>
+            <div class="flex flex-row justify-between">
+              <label class="block text-gray-700 text-lg" :for="field">{{field}}</label>
+              <aside class="text-sm text-gray-500">{{templates[addTileName][field].description}}</aside>
+            </div>
+            <input
+              :id="field"
+              :placeholder="templates[addTileName][field].example"
+              v-model="formData[field]"
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-2 mb-4"
+              required
+            />
+          </div>
+        </div>
+        <button
+          @click="validateForm()"
+          class="bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >Add Tile</button>
       </div>
     </div>
 
@@ -27,6 +81,8 @@ import 'firebase/database';
 import 'firebase/auth';
 import TileSlot from '@/components/TileSlot.vue';
 
+const tileTemplates = require('@/tiles.json').tiles;
+
 export default Vue.extend({
   name: 'Grid',
   props: {},
@@ -35,29 +91,63 @@ export default Vue.extend({
   },
   data() {
     return {
+      templates: {} as any,
       justify: 'justify-center',
       tileObject: [] as any,
       rows: 0,
       columns: 0,
       showPicker: false,
+      showConfirm: false,
       currentTileIndex: 0,
+      addTileName: '',
+      formData: {},
+      formError: [] as any,
     };
   },
   methods: {
     addTile(tileIndex: number) {
-      firebase.database().ref('/tiles').once('value', (snapshot) => {
-        const tileList = snapshot.val();
-      });
+      firebase
+        .database()
+        .ref('/tiles')
+        .once('value', (snapshot) => {
+          const tileList = snapshot.val();
+        });
       this.currentTileIndex = tileIndex;
       this.showPicker = true;
+    },
+    confirmTile(tileName: any) {
+      this.showPicker = false;
+      this.showConfirm = true;
+      this.addTileName = tileName;
+    },
+    validateForm() {
+      let fieldList = Object.keys(this.templates[this.addTileName]);
+      fieldList = fieldList.filter(field => field !== 'info');
+      const currentFields = Object.keys(this.formData);
+      this.formError = [];
+      fieldList.forEach((field) => {
+        if (!currentFields.includes(field)) {
+          this.formError.push(`missing ${field}`);
+        }
+      });
+      if (this.formError.length === 0) {
+        console.log('no errors found, add to db');
+        this.showConfirm = false;
+      }
     },
     quitModal(event: any) {
       if (event.target.parentElement === this.$el) {
         this.showPicker = false;
       }
     },
+    quitConfirm(event: any) {
+      if (event.target.parentElement === this.$el) {
+        this.showConfirm = false;
+      }
+    },
   },
-  mounted() {
+  async mounted() {
+    this.templates = await tileTemplates;
     const self = this;
     const { currentUser } = firebase.auth();
     if (currentUser) {
@@ -106,6 +196,6 @@ export default Vue.extend({
 
 <style scoped>
 div.blur {
-    background-color: rgba(0, 0, 0, .5);
+  background-color: rgba(0, 0, 0, 0.5);
 }
 </style>
